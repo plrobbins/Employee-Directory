bfe03dc25197b75d6a862c43407302e1ce193136
Philip Robbins <plrobbins68@outlook.com>
2020-12-02 19:51:04 -0500

    updating the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Title from "./components/Title"
+import Employee from "./components/Employees";
+import Form from "./components/Form"
 import './App.css';
+import orderBy from 'lodash/orderBy';
+import employees from './employees.json';
+import Wrapper from './components/Wrapper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees,
+    sortedEmployees: employees,
+    ascending: true
+  };
+
+  //use lodash to create function to sort by name
+  sortName = () => {
+    const { sortedEmployees, ascending } = this.state
+    console.log(sortedEmployees);
+    console.log(ascending);
+    const direction = ascending ? 'asc' : 'desc'
+    const sorted = orderBy(sortedEmployees, 'name', direction);
+    console.log(sorted)
+    this.setState({ sortedEmployees: sorted, ascending: !ascending })
+
+    console.log("clicked name to sort")
+  }
+
+
+  render() {
+    return (
+      <Wrapper>
+        <div className="container">
+        <Title>Employee Directory</Title>
+
+        
+          <Form />
+          <table className="table" id="table">
+            <thead>
+              <tr className="header">
+                <th scope="col" onClick={this.sortName}>Name</th>
+                <th scope="col">Department</th>
+                <th scope="col" >Phone Number</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            {this.state.sortedEmployees.map(employee => {
+              return (
+                <Employee
+                  id={employee.id}
+                  key={employee.id}
+                  name={employee.name}
+                  department={employee.department}
+                  phone={employee.phone}
+                  email={employee.email}
+                />
+              )
+            })}
+          </table>
+        </div>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
